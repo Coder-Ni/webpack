e5fbc13d909f0befd14c02ce7f4dede0d842dd50
Nichen1019 <1140295438@qq.com>
2022-07-12 22:49:33 +0800

    importLoaders属性
    css引入其他css文件时在option中添加importLoaders

--- 02_webpack_config/webpack.config.js
+++ 02_webpack_config/webpack.config.js
@@ -25,6 +25,9 @@ module.exports = {
 
           {
             loader: "css-loader",
+            options: {
+              importLoaders:1
+            }
           },
           // {
           //   loader: "postcss-loader",
